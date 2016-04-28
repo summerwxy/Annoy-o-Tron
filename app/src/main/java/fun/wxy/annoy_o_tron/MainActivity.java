2c43e028279074abf2600a1547e941c561bfe0a3
0_o <summer.wxy@gmail.com>
2016-04-28 11:09:40 +0800

    practice recycler view

--- app/src/main/java/fun/wxy/annoy_o_tron/MainActivity.java
+++ app/src/main/java/fun/wxy/annoy_o_tron/MainActivity.java
@@ -9,16 +9,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.androidannotations.annotations.EActivity;
-
+import fun.wxy.annoy_o_tron.list.Contact;
 import fun.wxy.annoy_o_tron.utils._;
 
 
@@ -80,29 +80,27 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Float action button Clicked!", Toast.LENGTH_LONG).show();
+                // Toast.makeText(MainActivity.this, "Float action button Clicked!", Toast.LENGTH_LONG).show();
+                Snackbar.make(contentView, "I am snackbar. a very very very very very very very very very very very very very very very very long snackbar.", Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                // do something.
+                            }
+                        }).show();
             }
         });
 
 
+        // adapter and recyclerview
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.recyclerView);
 
-        /*
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Hello !!!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
+        Contact.ContactsAdapter adapter = new Contact().new ContactsAdapter(Contact.generateSampleList());
+        rvContacts.setAdapter(adapter);
+        rvContacts.setLayoutManager(new GridLayoutManager(this, 3));
     }
-/*
+
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/fun/wxy/annoy_o_tron/list/Contact.java
@@ -0,0 +1,84 @@
+package fun.wxy.annoy_o_tron.list;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fun.wxy.annoy_o_tron.R;
+
+/**
+ * Created by 0_o on 2016/4/28.
+ */
+public class Contact {
+    // model
+    public class ContactModel {
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        private String name;
+    }
+
+    // generate tool
+    public static List<ContactModel> generateSampleList() {
+        List<ContactModel> result = new ArrayList<>();
+        for (int i = 0; i < 200; i++) {
+            ContactModel it = new Contact().new ContactModel();
+            it.setName("Name - " + i);
+            result.add(it);
+        }
+        return result;
+    }
+
+    // adapter
+    public class ContactsAdapter extends RecyclerView.Adapter<Contact.ContactsAdapter.ViewHolder> {
+        // view holder
+        public class ViewHolder extends RecyclerView.ViewHolder{
+            public TextView nameTextView;
+            public ViewHolder(View itemView){
+                super(itemView);
+
+                nameTextView = (TextView) itemView.findViewById(R.id.tv_name);
+            }
+        }
+        // constructer & getItemCount
+        private List<ContactModel> mContacts;
+
+        public ContactsAdapter(List<ContactModel> contacts){
+            mContacts = contacts;
+        }
+
+        @Override
+        public int getItemCount() {
+            return mContacts.size();
+        }
+
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            Context context = viewGroup.getContext();
+            View contactView = LayoutInflater.from(context).inflate(R.layout.item_contact, viewGroup, false);
+
+            ViewHolder viewHolder = new ViewHolder(contactView);
+
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, int position) {
+            ContactModel contact = mContacts.get(position);
+            TextView nameTextView = viewHolder.nameTextView;
+            nameTextView.setText(contact.getName());
+        }
+    }
+}
